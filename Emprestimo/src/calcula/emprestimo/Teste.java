5977d966243893dc936a409e8e574207ea5cac6b
StanleyKenneth <kstanley_bass@hotmail.com>
2023-01-29 13:27:41 -0300

    Correção nome pasta

---
+++ Emprestimo/src/calcula/emprestimo/Teste.java
@@ -0,0 +1,31 @@
+package calcula.emprestimo;
+
+import java.math.BigDecimal;
+
+public class Teste extends Moeda {
+
+	public Teste(String valor) {
+		super(valor);
+		
+	}
+
+	public static void main(String[] args) {
+	 
+	/*	Moeda salario = new Moeda("100000");
+		salario.subtrair("60.00");
+		salario.somarCom("300.00");
+		salario.multiplicar("0.10");
+		salario.dividir(new BigDecimal("1.3"));
+		System.out.println(salario.valorFormatado());*/
+		
+		double salario = 3000;
+		double pretacão;
+		
+		 
+		salario = salario * 0.2;
+		System.out.println(salario);
+		
+	
+	}
+
+}
