9ab2f78935e6f7c7ecfe92777d6fc04d347e86d9
StanleyKenneth <kstanley_bass@hotmail.com>
2023-01-29 15:25:40 -0300

    Corrigindo o formato de apresentação dos valores em reais.

---
+++ Emprestimo/src/calcula/emprestimo/FORMATO.java
@@ -0,0 +1,13 @@
+package calcula.emprestimo;
+
+import java.math.BigDecimal;
+
+public interface FORMATO {
+
+	public String valorFormatado();
+
+	public static String format(BigDecimal valor) {
+	
+		return null;
+	}
+}
